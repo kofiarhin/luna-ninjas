6e94f0d8d59da02e5cc2b8fb593f2ea23a5c93ab
kofi arhin <kofiarhin69@gmail.com>
2025-11-15 16:56:13 +0000

    initial server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("server started");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res, next) => {
+  return res.json({ message: "hello world" });
+});
+app.listen(PORT, () => {
+  console.log("server started on port:", PORT);
+});
